0f63226d10036b1e033bc3714e6a0716ff6f91a5
Maziyar Gerami <maziyar.grami@gmail.com>
2015-12-25 00:39:28 +0330

    Commit for ./BBO/GetLambdaMu.m

---
+++ BBO/GetLambdaMu.m
@@ -0,0 +1,16 @@
+function [lambda, mu] = GetLambdaMu(Population, I, E)
+
+% Compute immigration rate and extinction rate for each species count.
+% lambda(i) is the immigration rate for individual i.
+% mu(i) is the extinction rate for individual i.
+
+
+
+for i = 1 : length(Population)
+    
+    mu(i) = I * (1 - i / length(Population));
+    
+    lambda(i) = E * (i / length(Population));
+    
+end
+end
